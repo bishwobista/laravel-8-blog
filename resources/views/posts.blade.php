dff5b00af8fe4f9464e95a92d12dbba56076c5d0
bishwo bista <bishwo5bista@gmail.com>
2024-01-16 11:26:49 +0545

    blog post page

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,6 +1,7 @@
 <x-layout>
 
     @include('_posts-header')
+
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
         @if($posts->count())
             <x-posts-grid :posts="$posts"/>
@@ -8,20 +9,4 @@
             <p class="text-center"> No posts yet, please check back later</p>
         @endif
     </main>
-    {{--          @foreach($posts as $post)
-            <article>
-                <h1>
-                    <a href="/posts/{{$post->slug}}">
-                        {!! $post->title!!}
-                    </a>
-                </h1>
-                <p>
-                    By: <a href="/users/{{$post->user->username}}">{{$post->user->name}}</a> <br>
-                    <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>
-                </p>
-                <div>
-                    {{$post->excerpt}}
-                </div>
-            </article>
-        @endforeach   --}}
 </x-layout>
