315cc061e61fa9ede2ce86d17bd42ccb236e8e99
bishwo bista <bishwo5bista@gmail.com>
2024-01-16 14:37:14 +0545

    search query

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Models;
+
 use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -8,15 +9,30 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
-    protected $guarded= [];
+
+    protected $guarded = [];
+
 //    protected $fillable= ['title', 'excerpt', 'body'];
 
-public function category(){
-    return $this->belongsTo(Category::class);
-}
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 
-public function user()
-{
-    return $this->belongsTo(User::class);
-}
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function scopeFilter($query, array $filters){
+        $query->when($filters['search'] ?? false, fn ($query, $search)=>
+
+
+            $query->where('title', 'like', '%'. $search . '%' )
+                ->orWhere('body',  'like', '%'. $search . '%')
+
+        );
+    }
+
+
 }
